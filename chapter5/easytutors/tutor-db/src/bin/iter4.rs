d10eb7806836e0c3cdfb802eca1503594dcc7004
soarewee <128680097+soarewee@users.noreply.github.com>
2025-02-04 14:01:24 +0000

    custom error definition

--- chapter5/easytutors/tutor-db/src/bin/iter4.rs
+++ chapter5/easytutors/tutor-db/src/bin/iter4.rs
@@ -5,15 +5,15 @@ use std::env;
 use std::io;
 use std::sync::Mutex;
 
-#[path = "../iter3/db_access.rs"]
+#[path = "../iter4/db_access.rs"]
 mod db_access;
-#[path = "../iter3/handlers.rs"]
+#[path = "../iter4/handlers.rs"]
 mod handlers;
-#[path = "../iter3/models.rs"]
+#[path = "../iter4/models.rs"]
 mod models;
-#[path = "../iter3/routes.rs"]
+#[path = "../iter4/routes.rs"]
 mod routes;
-#[path = "../iter3/state.rs"]
+#[path = "../iter4/state.rs"]
 mod state;
 
 use routes::*;
---
+++ chapter5/easytutors/tutor-db/src/iter4/errors.rs
@@ -0,0 +1,35 @@
+use actix::web::{error, http::StatusCode, HttpResponse, Result};
+use serde::Serialize;
+use sqlx::error::Error as SQLxError;
+use std::fmt;
+
+#[derive(Debug, Serialize)]
+pub enum EzyTutorError {
+    DBError(String),
+    ActixError(String),
+    NotFound(String),
+}
+
+#[derive(Debug, Serialize)]
+pub struct MyErrorResponse {
+    error_message: String,
+}
+
+impl EzyTutorError {
+    fn error_response(&self) -> String {
+        match self {
+            EzyTutorError::DBError(msg) => {
+                println!("Database error occurred: {:?}", msg);
+                "Database error".into()
+            }
+            EzyTutorError::ActixError(msg) => {
+                println!("Server error occurred: {:?}", msg);
+                "Internal Server error".into()
+            }
+            EzyTutorError::NotFound(msg) => {
+                println!("Not Found error occurred: {:?}", msg);
+                msg.into()
+            }
+        }
+    }
+}
